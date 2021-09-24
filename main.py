b2ca94cde5e861524e4b187df280ae146271b001
Divider <89965004+it-cio@users.noreply.github.com>
2021-09-24 10:02:06 +0300

    Added weather request

--- main.py
+++ main.py
@@ -5,18 +5,6 @@ from aiogram import executor, types
 from info.weather import weather_request
 
 
-# @cfg.dp.message_handler(commands=['search'])
-# async def bot_answer(message: types.Message):
-#     answer = name_recognition(message)
-#     await cfg.bot.send_message(cfg.user_id, answer)
-#     try:
-#         await cfg.bot.delete_message(cfg.bot_id, cfg.search_id)
-#     except:
-#         await cfg.bot.delete_message(cfg.user_id, cfg.search_id)
-#     finally:
-#         await cfg.asyncio.sleep(1)
-
-
 @cfg.dp.message_handler(content_types=['pinned_message'])
 async def delete_pinned(message: types.Message):
     await message.delete()
@@ -31,23 +19,20 @@ async def scheduler():
 
 async def on_startup(_):
     cfg.asyncio.create_task(scheduler())
-    await cfg.bot.send_message(cfg.bot_id, 'Бот работает в тестовом режиме!')
+    bot_message = await cfg.bot.send_message(cfg.bot_id, 'Бот работает в тестовом режиме!')
+    cfg.test_message_id = bot_message.message_id
     await cfg.asyncio.sleep(1)
 
 
-'''
 async def on_shutdown(_):
-    sql_id = cfg.connection.execute(cfg.text(f'SELECT weather_id, covid_id FROM info;')).fetchone()
-    if sql_id[0] != '0000':
-        await cfg.bot.delete_message(cfg.bot_id, sql_id[0])
-    if sql_id[1] != '0000':
-        await cfg.bot.delete_message(cfg.bot_id, sql_id[1])
-    cfg.connection.execute(cfg.text(f'UPDATE info SET weather_id = "0000", covid_id = "0000";'))
-    await asyncio.sleep(1)
-'''
+    await cfg.bot.delete_message(cfg.bot_id, cfg.test_message_id)
+    for message_id in cfg.weather_message_id:
+        await cfg.bot.delete_message(cfg.bot_id, message_id)
+    await cfg.asyncio.sleep(1)
+
 
 if __name__ == '__main__':
     try:
-        executor.start_polling(cfg.dp, skip_updates=True, on_startup=on_startup)  # on_shutdown=on_shutdown
+        executor.start_polling(cfg.dp, skip_updates=True, on_startup=on_startup, on_shutdown=on_shutdown)
     except Exception as ex:
         print(f'({cfg.date} {cfg.time}) AioGram-Error: {ex}')
