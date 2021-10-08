ed8cac0cd5a5f8570f68cb1644c0bc13630a45c0
Divider <it-cio@mail.ru>
2021-10-08 16:14:48 +0300

    ver 0.5

--- main.py
+++ main.py
@@ -1,21 +1,75 @@
+import asyncio
 import aioschedule
-import routes
+
+from data import sql
+from asterisk import ami
+from telegram import bot
+from covid.prognosis import covid_request
+from weather.forecast import weather_request
+
+
+async def weather(greet):
+    forecast = await weather_request()
+    sql_forecast, sql_id = await asyncio.create_task(sql.select('weather'))
+
+    if forecast != sql_forecast:
+        try:
+            weather_id = sql_id
+            await bot.edit_message(greet + forecast, weather_id)
+        except Exception as ex:
+            print(f'{ex} (id: {sql_id})')
+            weather_id = await bot.send_message(greet + forecast)
+            await bot.pin_message(weather_id)
+        finally:
+            await asyncio.create_task(sql.update('weather', forecast, weather_id))
+            await asyncio.sleep(1)
+
+
+async def covid(greet):
+    prognosis = await covid_request()
+    sql_prognosis, sql_id = await asyncio.create_task(sql.select('covid'))
+
+    if prognosis != sql_prognosis:
+        try:
+            covid_id = await bot.send_message(greet + prognosis)
+            await asyncio.create_task(sql.update('covid', prognosis, covid_id))
+        except Exception as ex:
+            print(ex)
+        finally:
+            await asyncio.sleep(1)
+
+
+async def ami_listener():
+    ami.connect(state=False)
+    while True:
+        if ami.event:
+            print(ami.event[0])
+            ami.event = []
+        elif ami.caller and ami.number:
+            call_id = await bot.send_message(f'Incoming call\nfrom number: {ami.number[0]}\nto number: {ami.caller[0]}')
+            ami.number, ami.caller = [], []
+        elif ami.status:
+            await bot.delete_message(call_id)
+            bot.id_list.remove(call_id)
+            ami.status = []
+        await asyncio.sleep(1)
 
 
 # choose a time interval to run functions
 async def scheduler():
-    aioschedule.every(15).to(30).seconds.do(routes.route_weather, greet='Прогноз погоды: ')
-    aioschedule.every(30).seconds.do(routes.route_covid, greet='Коронавирус\nоперативные данные\n\n')
+    aioschedule.every(15).to(30).seconds.do(weather, greet='Прогноз погоды: ')
+    aioschedule.every(30).seconds.do(covid, greet='Коронавирус\nоперативные данные\n\n')
     while True:
         await aioschedule.run_pending()
-        await routes.asyncio.sleep(1)
+        await asyncio.sleep(1)
 
 
 async def tasks():
-    routes.asyncio.create_task(scheduler())
-    await routes.asyncio.sleep(1)
-routes.asyncio.get_event_loop().run_until_complete(tasks())
+    asyncio.create_task(scheduler())
+    asyncio.create_task(ami_listener())
+    await asyncio.sleep(1)
+asyncio.get_event_loop().run_until_complete(tasks())
 
 
 if __name__ == '__main__':
-    routes.bot_run()
+    bot.run(startup=sql.create_table, shutdown=sql.drop_table)
